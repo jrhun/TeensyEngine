16d8d07d972d67b6debcaa90e90b76b98c286b55
jrhun <j.r.l.hunter@gmail.com>
2019-08-23 17:04:33 +1000

    added GUI

--- src/Particle.h
+++ src/Particle.h
@@ -1,7 +1,20 @@
 #pragma once
 
 #include "Vec3.h"
+#include "Color.h"
 
 class Particle P {
+	Particle() {}
+	Particle(Particle &p) : pos(p.pos), vel(p.vel), acc(p.acc), alpha(p.alpha), col(p.col) {}
 
+	void addForce(Vec3 f) {
+
+	}
+
+	Vec3 pos;
+	Vec3 vel;
+	Vec3 acc;
+	float mass; 
+	float alpha;
+	CRGB col;
 };
